fb75ad75fdf6e42da1c5a35240fc8d9d7ab54f63
Justin Restivo <jrestivo@mit.edu>
2019-05-28 12:15:52 -0400

    current working version

--- buildscripts/paths.sh
+++ buildscripts/paths.sh
@@ -1,5 +1,6 @@
 #QEMU_RISCV=/home/dieraca/school/fall_2018/urop/riscv-qemu/riscv32-softmmu/qemu-system-riscv32
-QEMU_RISCV=/home/dieraca/school/fall_2018/urop/riscv-qemu/riscv32-softmmu/qemu-system-riscv32
+QEMU_RISCV=/home/dieraca/school/fall_2018/urop/qemu/riscv32-softmmu/qemu-system-riscv32
+#QEMU_RISCV=/home/dieraca/school/fall_2018/urop/qemuvanilla/riscv32-softmmu/qemu-system-riscv32
 #QEMU_RISCV=qemu-system-riscv32
 TOCK_PATH=/home/dieraca/school/fall_2018/urop/tockos/
 BOARD_PATH=$TOCK_PATH/boards/hifive1
--- buildscripts/run.sh
+++ buildscripts/run.sh
@@ -1,8 +1,9 @@
 source ./paths.sh
 sudo pkill qemu-system-ris
+rm ./log
 
 #sudo $QEMU_RISCV  -s -S -kernel $OUT_PATH/hifive1-app.elf &
-sudo $QEMU_RISCV -machine sifive_u -cpu sifive-e31 -s -S -kernel $OUT_PATH/hifive1-app.elf &
+sudo $QEMU_RISCV -machine sifive_u -cpu sifive-e31 -s -S -kernel $OUT_PATH/hifive1-app.elf >> ./log &
 #sudo $QEMU_RISCV -cpu sifive-e31 -s -S -kernel $OUT_PATH/hifive1-app.elf &
 sleep 1
 
